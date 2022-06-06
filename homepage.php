54d31852c6c2216077cd5ea0af9145386a818487
Robin Rawat <robinrwt85@gmail.com>
2022-06-07 00:12:45 +0530

    store song details in database

--- homepage.php
+++ homepage.php
@@ -2,13 +2,13 @@
     $showerror=false;
     $showmsg=false;
     require './database_connection.php';
-    if($_SERVER['REQUEST_METHOD']=="POST")
+    if($_SERVER['REQUEST_METHOD']=="GET")
     {
-        if(isset($_POST['artist_name']) && isset($_POST['DOB']) && isset($_POST['bio']))
+        if(isset($_GET['artist_name']) && isset($_GET['DOB']) && isset($_GET['bio']))
         {
-            $A_name=$_POST['artist_name'];
-            $DOB=$_POST['DOB'];
-            $bio=$_POST['bio'];
+            $A_name=$_GET['artist_name'];
+            $DOB=$_GET['DOB'];
+            $bio=$_GET['bio'];
 
             $q="SELECT * from artist WHERE artist_name='$A_name' AND date_of_birth='$DOB' AND bio='$bio' ";
             $result=mysqli_query($conn,$q);
@@ -37,6 +37,70 @@
     }
     mysqli_close($conn);
     ?>
+
+ <?php
+  $path="./Artwork/";
+  if(!file_exists($path))
+  {
+      mkdir($path);
+  }
+  $artworkpath="Artwork/";
+  if($_SERVER['REQUEST_METHOD']=='POST')
+  {
+      if( isset($_POST['song_name']) && isset($_POST['released_date']) && isset($_POST['artists']) && isset($_FILES['artwork']))
+      {
+            $songName=$_POST['song_name'];
+            $date=$_POST['released_date'];
+            $artwork=$_FILES['artwork'];
+            $artistName=$_POST['artists'];
+          
+        //   if($_FILES['artwork']['size']==0)
+        //   {
+        //       $_FILES['artwork']['name']="default_artwork.jpg";
+        //   }
+          $artworkName= $artworkpath.basename($_FILES['artwork']['name']);
+          #checking if song already exist or not
+          require 'database_connection.php';
+          $quer1="SELECT * FROM songs WHERE song_name = '$songName' AND date_released='$date' AND singer= '$artistName' ";
+          $result=mysqli_query($conn,$quer1);
+          $rows=mysqli_num_rows($result);
+          if($rows>0)
+          {
+              $showerror="Song Already Exist";
+          }
+          else
+          {    
+              $result1=move_uploaded_file($_FILES['artwork']['tmp_name'],$artworkName);
+              if($result1)
+              {
+                 $query2="INSERT INTO songs (song_name,date_released,artwork,singer) VALUES(?,?,?,?)";
+                 $stmt=mysqli_prepare($conn,$query2);
+                 $success=mysqli_stmt_bind_param($stmt,"ssss",$songName,$date,$artworkName,$artistName);
+                 if($success)
+                 {
+                   
+                    $result3=mysqli_stmt_execute($stmt);
+                    if($result3)
+                    {
+                        $showmsg="Successfully added.";
+                    }
+                    else
+                    {
+                        $showerror="Unable to add Book.";
+                    }
+                 }
+              }
+          }
+         
+      }
+      else
+      {
+          $showerror="inputed field can not be empty";
+      }
+  }   
+ ?>
+ 
+ 
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -124,22 +188,22 @@
 <!-- add new song section -->
     <section class="addsongdetails flex">
         <h1 style="font-size:2.5rem;padding:2rem 4rem">Adding a New Song -</h1>
-        <form class="flex formdata" action="#" method="POST">
+        <form class="flex formdata" action="#" method="POST" enctype="multipart/form-data">
             <div class="flex field">
                 <label for="sname">Song Name </label>
-                <input class="input" type="text" name="song_name" id="sname">
+                <input class="input" type="text" name="song_name" id="sname" required>
             </div>
             <div class="flex field">
                 <label for="date">Date Released </label>
-                <input  class="input" type="date" name="released_date" id="date">
+                <input  class="input" type="date" name="released_date" id="date" required>
             </div>
             <div class="flex field">
                 <label for="photo">Artwork </label>
-                <input class="input" type="file" name="artwork" id="photo" value="Upload Image">
+                <input class="input" type="file" name="artwork" id="photo" value="Upload Image" accept="image/*" required>
             </div>
             <div class="flex field">
                 <label for="aname">Artist </label>
-                <input class="input" type="text" name="artists[]" list="aname">
+                <input class="input" type="text" name="artists" list="aname" required>
                 <datalist id="aname">
                 <?php
                     require './database_connection.php';
@@ -158,7 +222,7 @@
                 </datalist>
             </div>
             <div style="text-align: center; margin-top: 3rem;">
-                <input class="button" type="button" value="Save">
+                <input class="button" type="submit" value="Save">
                 <input class="button" type="reset" value="cancel">
              </div>
         </form>
@@ -171,7 +235,7 @@
             <h1>Add Artist -</h1>
             <button class="cross">&#9747</button>
         </div>
-        <form class="flex artistdata" action="<?php htmlspecialchars($_SERVER['PHP_SELF']);?>" method="POST">
+        <form class="flex artistdata" action="<?php htmlspecialchars($_SERVER['PHP_SELF']);?>" method="GET">
             <div class="flex field">
                 <label for="AN">Artist Name </label>
                 <input class="input" type="text" name="artist_name" id="AN">
